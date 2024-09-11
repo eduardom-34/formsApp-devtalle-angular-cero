709e2e2d8a61c87682b95aebc67466bb0d145782
CesarLinux <eduardomelchor34@gmail.com>
2024-09-10 20:02:59 -0600

    Fixing Dinamic to Dynamic, component and file name

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
-import { DinamicPageComponent } from './pages/dinamic-page/dinamic-page.component';
+
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 
 
 @NgModule({
   declarations: [
     BasicPageComponent,
-    DinamicPageComponent,
-    SwitchesPageComponent
+    SwitchesPageComponent,
+    DynamicPageComponent
   ],
   imports: [
     CommonModule,
