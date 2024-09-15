9a425bed57e421107b2e374f61576bb546c86d30
edu Kubuntu <eduardomelchor34@gmail.com>
2024-09-14 21:26:42 -0600

    Reestablecer y establecer valor de formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+
+const rtx5090 = {
+  name: 'TRX 5090',
+  inStorage: 2500,
+  price: 6
+}
+
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css'
 })
-export class BasicPageComponent {
+export class BasicPageComponent  implements OnInit {
 
   // public myform: FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -16,7 +24,7 @@ export class BasicPageComponent {
 
   public myForm: FormGroup;
 
-  constructor( private fb: FormBuilder ) {
+  constructor( private fb: FormBuilder )  {
     this.myForm = this.fb.group({
       name: ['', [Validators.required,  Validators.minLength(3)]],
       price: [0, [Validators.required, Validators.min(0)]],
@@ -24,11 +32,18 @@ export class BasicPageComponent {
     })
   }
 
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
+
   onSave():void {
 
     if ( this.myForm.invalid ) return;
 
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+
   }
 
 }
