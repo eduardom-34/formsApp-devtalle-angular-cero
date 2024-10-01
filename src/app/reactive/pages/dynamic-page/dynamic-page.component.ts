4a92d46b01549abea022c000610b0ebadec73d33
edu Kubuntu <eduardomelchor34@gmail.com>
2024-09-30 20:02:39 -0600

    Formulario Dinamicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './dynamic-page.component.css'
 })
 export class DynamicPageComponent {
+
+
+  public myForm: FormGroup;
+
+
+  // public myForm: FormGroup = this.fb.group({
+  //   name: ['', [Validators.required, Validators.minLength(3)]],
+  //   favoriteGames: this.fb.array([])
+  // })
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      favoriteGames: this.fb.array([
+        ['Metal Gear', Validators.required],
+        ['Death Stranding', Validators.required],
+      ])
+    })
+  }
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+
+  onSubmit():void {
+    if ( this.myForm.invalid ){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }
