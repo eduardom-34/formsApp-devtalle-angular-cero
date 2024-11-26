55d0d6ca8a7d01c36246962b03efa6f8866eb9ed
edu Kubuntu <eduardomelchor34@gmail.com>
2024-11-26 15:25:17 -0600

    form validators with our own validators

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators.functions';
 
 @Component({
   selector: 'app-register-page',
@@ -15,7 +16,7 @@ export class RegisterPageComponent {
     this.myForm = this.fb.group({
       name: ['', [Validators.required]],
       email: ['', [ Validators.required]],
-      username: ['', [Validators.required]],
+      username: ['', [Validators.required, cantBeStrider ]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       password2: ['', [Validators.required]]
     });
